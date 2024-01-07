c5f2e5205d5617fead8495e8f8334718cc898fc0
Andrea Zappa <andre18.zappa@gmail.com>
2024-01-07 16:07:49 +0100

    Structure Change
    modified the html structure, created element via js, added keyboard functionality

---
+++ assets/script/elements.js
@@ -0,0 +1,194 @@
+const container = document.querySelector("#container");
+// console.log(container);
+
+// Creation of all the HTML elements
+
+// Cretion of startContainer where the user can set a specific starting value for the counter
+const startContainer = createElement(
+  "div",
+  "startContainer",
+  ["start"],
+  null,
+  container
+);
+
+//  Label for the start input
+const startInputLabel = createElement(
+  "label",
+  null,
+  [null],
+  "Start From:",
+  startContainer,
+  { for: "startInput" }
+);
+
+//  Iinput Element
+const startInput = createElement(
+  "input",
+  "startInput",
+  [null],
+  null,
+  startContainer,
+  { type: "text", placeholder: "Insert a start value" }
+);
+
+// Set Counter Button
+const setCounterButton = createElement(
+  "button",
+  "setCounter",
+  "setCounterButton",
+  "Set Counter <p>Enter</p>",
+  startContainer
+);
+
+// Creation of the counter card
+
+// Counter Container
+const counterContainer = createElement(
+  "div",
+  "counterContainer",
+  ["counterContainer"],
+  null,
+  container
+);
+
+// Counter Display
+const counterDisplay = createElement(
+  "div",
+  "counterDisplay",
+  ["counterDisplay"],
+  "0",
+  counterContainer
+);
+
+// Counter Buttons
+const containerCounterButtons = createElement(
+  "div",
+  "containerButtonsCounter",
+  ["containerButtonsCounter"],
+  null,
+  counterContainer
+);
+
+// Minus Button
+const minusButton = createElement(
+  "button",
+  "minusButton",
+  ["minusButton", "buttonCounter"],
+  "-",
+  containerCounterButtons,
+  { onclick: "minus()" }
+);
+
+// Plus Button
+const plusButton = createElement(
+  "button",
+  "plusButton",
+  ["plusButton", "buttonCounter"],
+  "+",
+  containerCounterButtons,
+  { onclick: "plus()" }
+);
+
+// Creation of other buttons
+
+// Container
+const otherButtonsContainer = createElement(
+  "div",
+  "otherButtons",
+  ["otherButtons"],
+  null,
+  container
+);
+
+// Auto Button
+const autoButtons = createElement(
+  "button",
+  "autoButton",
+  ["autoButton"],
+  'Auto <p id="shortAutoButton">A</p>',
+  otherButtonsContainer
+);
+
+// Reset Button
+const resetButtons = createElement(
+  "button",
+  "resetButton",
+  ["resetButton"],
+  "Reset <p>Canc</p>",
+  otherButtonsContainer,
+  { onclick: "reset()" }
+);
+
+// Counter Functionality
+let counter = 0;
+let interval;
+
+// Increment by 10 on long pression only if the counter is a two-digit number
+document
+  .querySelector(".plusButton")
+  .addEventListener("mousedown", function () {
+    if (counter > 9 || counter < -9) {
+      interval = setInterval(plus10, 500);
+    } else {
+      interval = setInterval(plus, 500);
+    }
+  });
+
+// Decrement of 10 by long pression only if the counter is a two-digit number
+document
+  .querySelector(".minusButton")
+  .addEventListener("mousedown", function () {
+    if (counter > 9 || counter < -9) {
+      interval = setInterval(minus10, 500);
+    } else {
+      interval = setInterval(minus, 500);
+    }
+  });
+
+// Stop the counter on mouseup
+document.addEventListener("mouseup", function () {
+  clearInterval(interval);
+});
+
+// Auto Increment by Auto button
+document.getElementById("autoButton").addEventListener("click", function () {
+  autoCounter();
+});
+
+// Short text for auto increment button
+const shortAutoButtonText =
+  document.getElementById("shortAutoButton").outerHTML;
+
+// Setting a start value by Set button
+document.querySelector("#setCounter").addEventListener("click", function () {
+  const startValue = parseInt(document.getElementById("startInput").value);
+
+  if (!isNaN(startValue)) {
+    counter = startValue;
+    document.getElementById("counter").innerHTML = counter;
+  } else {
+    alert("Please enter a valid number."); // Alert message in case of invalid input
+  }
+  document.getElementById("startInput").value = ""; // Reset the input
+});
+
+// Keyboard Input
+
+// Setting a start value via keyboard
+document
+  .getElementById("startInput")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault(); // Prevents form submission from reloading the page (default behavior)
+      const startValue = parseInt(this.value);
+
+      if (!isNaN(startValue)) {
+        counter = startValue;
+        document.getElementById("counterDisplay").innerHTML = counter;
+      } else {
+        alert("Please enter a valid number."); // Show a message if the input is not a valid number
+      }
+      this.value = ""; // Resets the input value after the counter is updated
+    }
+  });
--- assets/script/functions.js
+++ assets/script/functions.js
@@ -1,46 +1,88 @@
+// Create Element
+function createElement(
+  typeElem,
+  idElem,
+  classElem,
+  contentElem,
+  parentToAppend,
+  additionalAttributes
+) {
+  let elem = document.createElement(typeElem);
+  elem.setAttribute("id", idElem);
+  elem.classList.add(...classElem);
+  elem.innerHTML = contentElem;
+  parentToAppend.append(elem);
+
+  if (additionalAttributes) {
+    for (const attribute in additionalAttributes) {
+      elem.setAttribute(attribute, additionalAttributes[attribute]);
+    }
+  }
+  return elem;
+}
+
 // Increment Function
 function plus() {
   counter++;
-  document.getElementById("counter").innerHTML = counter;
+  document.getElementById("counterDisplay").innerHTML = counter;
 }
 
 // Decrement Function
 function minus() {
   counter--;
-  document.getElementById("counter").innerHTML = counter;
+  document.getElementById("counterDisplay").innerHTML = counter;
 }
 
 // Reset Function
 function reset() {
   counter = 0;
-  document.getElementById("counter").innerHTML = counter;
+  document.getElementById("counterDisplay").innerHTML = counter;
 }
 
 // Increment Function of 10
 function plus10() {
   counter += 10;
-  document.getElementById("counter").innerHTML = counter;
+  document.getElementById("counterDisplay").innerHTML = counter;
 }
 
 // Decremet Function of 10
 function minus10() {
   counter -= 10;
-  document.getElementById("counter").innerHTML = counter;
+  document.getElementById("counterDisplay").innerHTML = counter;
 }
 
 // Auto Increment Function
-const shortButtonText = document.getElementById("shortAutoButton").outerHTML;
 let autoInterval = false;
 
-function startAutoCounter() {
+function autoCounter() {
   if (!autoInterval) {
     autoInterval = setInterval(function () {
       plus();
-      autoButton.innerHTML = "Stop" + shortButtonText;
+      autoButton.innerHTML = "Stop" + shortAutoButtonText;
     }, 600);
   } else {
     clearInterval(autoInterval);
     autoInterval = false;
-    autoButton.innerHTML = "Auto" + shortButtonText;
+    autoButton.innerHTML = "Auto" + shortAutoButtonText;
   }
 }
+
+// Keyboard Input for counter
+document.addEventListener("keydown", function (event) {
+  switch (event.key) {
+    case "+":
+    case "ArrowUp":
+      plus();
+      break;
+    case "-":
+    case "ArrowDown":
+      minus();
+      break;
+    case "Delete":
+      reset();
+      break;
+  }
+  if (event.shiftKey && event.key === "A") {
+    autoCounter();
+  }
+});
